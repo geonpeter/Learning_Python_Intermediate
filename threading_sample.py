e62266fe128a5b0555e8a117f111ece9c28a141a
Geon Peter <igeonpeter@gmail.com>
2024-11-05 09:00:48 +0100

    Add threading.py

--- threading_sample.py
+++ threading_sample.py
@@ -1,18 +1,22 @@
 import threading
-def function1():
-    for i in range(10):
-        print("Function1")
-        
-def function2():
-    for j in range(10):
-        print("Function2")
-        
-t1 = threading.Thread(target=function1)
-t2 = threading.Thread(target=function2)
 
+def function_1():
+    for i in range(30):
+        print(f"Line No .{i} Function-1")
+        
+def function_2():
+    for j in range(30):
+        print(f"Line No .{j} Function-2")
+        
+t1 = threading.Thread(target=function_1)
+t2 = threading.Thread(target=function_2)
 
 t1.start()
 t2.start()
-t1.join()
+
 t2.join()
-print("!!!!!!!!!Function3!!!!!!!!!")
+t1.join()
+print("****** An interrupter -1  ******")
+print("****** An interrupter -2  ******")
+
+
